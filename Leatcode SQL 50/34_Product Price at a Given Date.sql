08c9a89fffa0310ef389a9ae7ddcf7ec6096a6b4
Shubham Balkrushna Kangune <shubhamcsc4656@gmail.com>
2026-01-09 22:38:11 +0530

    Product Price at a Given Date

---
+++ Leatcode SQL 50/34_Product Price at a Given Date.sql
@@ -0,0 +1,15 @@
+SELECT 
+    p.product_id,
+    COALESCE(
+        (
+            SELECT new_price
+            FROM Products p2
+            WHERE p2.product_id = p.product_id
+              AND p2.change_date <= '2019-08-16'
+            ORDER BY p2.change_date DESC
+            LIMIT 1
+        ),
+        10
+    ) AS price
+FROM Products p
+GROUP BY p.product_id;
